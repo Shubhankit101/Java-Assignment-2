6db4f8f80f5c6f9d891491ca7422ffa4f2d8bf54
Shubhankit Tiwari <shubhankit101@gmail.com>
2021-09-30 18:00:34 +0530

    29 September 2021

---
+++ NumberAppearOnce.java
@@ -0,0 +1,30 @@
+package geeksterAssignment2;
+
+import java.util.Scanner;
+
+public class NumberAppearOnce {
+
+	public static void main(String[] args) {
+		
+		int n; System.out.println("Enter Size of Array");
+		Scanner sc= new Scanner(System.in);
+		n=sc.nextInt();
+		System.out.println("Enter Elements of Array");
+		
+		int a[] =new int[n];
+		for(int i=0;n>i;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int temp=a[0];
+		for(int i=1;n>i;i++)
+		{
+			temp=temp^a[i];
+		}
+		System.out.println("Element occouring once is "+temp);
+		sc.close();
+		
+
+	}
+
+}
